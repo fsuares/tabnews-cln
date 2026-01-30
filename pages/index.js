4f5151a8cf32a9456ea99112b744c722fb2d217b
Fernando <feesuares@gmail.com>
2026-01-30 10:32:47 -0300

    feat: add home page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to TabNews</h1>
+      <p>This is a practice implementation of https://www.tabnews.com.br.</p>
+    </div>
+  );
+}
